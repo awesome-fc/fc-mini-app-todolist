0ae997b542d91e60f0f41b3e1d7d8b644ea6cc24
dehui.kdh <dehui.kdh@alibaba-inc.com>
2020-02-21 10:53:34 +0800

    fuzzy info

--- mini-app-todo-list-client/server.js
+++ mini-app-todo-list-client/server.js
@@ -1,11 +1,11 @@
 const util = require('util');
 const FClient = require('@alicloud/fc2');
 
-const fcAccountId = '1671011226853360';
-const fcRegion = 'cn-hangzhou';
-const fcServiceName = 'todo-list4-todo-list-svc-6ED644ABFC05';
-const sessionFunctionName = 'todo-list4-session-51E58506D625';
-const dbFunctionName = 'todo-list4-db-7A9F31268853';
+const fcAccountId = '<AccountId>';
+const fcRegion = '<Region>';
+const fcServiceName = '<ServiceName>';
+const sessionFunctionName = '<SessionFunctionName>';
+const dbFunctionName = '<DBFunctionName>';
 const sessionEndpoint = util.format('https://%s.%s.fc.aliyuncs.com/2016-08-15/proxy/%s/%s/',
                                     fcAccountId, fcRegion, fcServiceName, sessionFunctionName);
 
